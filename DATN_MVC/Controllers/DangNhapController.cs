088a65b81d7b08ab8847331a31d6b2bb79286011
TaDucNhuTrinh <tatrinh0102@gmail.com>
2025-02-13 14:30:43 +0700

    Giao điện

---
+++ DATN_MVC/Controllers/DangNhapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_MVC.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
