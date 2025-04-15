87e6ddb5736ae96527a8e4502d8237a2591afd06
TaDucNhuTrinh <tatrinh0102@gmail.com>
2025-04-15 09:58:38 +0700

    Giao Điện Thanh Toán

---
+++ DATN_MVC/Controllers/ThanhToanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_MVC.Controllers
+{
+    public class ThanhToanController : Controller
+    {
+        public IActionResult ThanhToan()
+        {
+            return View();
+        }
+    }
+}
