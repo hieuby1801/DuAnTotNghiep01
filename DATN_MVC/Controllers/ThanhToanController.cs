61cb463380ccfde0b4466e2fffb427d945133db4
nam975 <137900292+nam975@users.noreply.github.com>
2025-04-15 17:33:32 +0700

    them nưa 15/4

--- DATN_MVC/Controllers/ThanhToanController.cs
+++ DATN_MVC/Controllers/ThanhToanController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DATN_MVC.Controllers
 {
@@ -6,7 +7,17 @@ namespace DATN_MVC.Controllers
     {
         public IActionResult ThanhToan()
         {
-            return View();
+			var userId = User.Claims.FirstOrDefault(c => c.Type == "Id")?.Value;
+			if (userId == null)
+			{
+				return RedirectToAction("DangNhap", "DangNhap"); // hoặc xử lý theo ý bạn
+			}
+
+			// Dùng userId để lấy giỏ hàng của người dùng
+			
+
+			
+			return View();
         }
     }
 }
