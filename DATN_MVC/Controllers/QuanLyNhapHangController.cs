33c5977e03e377a5d63ea947770a122248290467
TaDucNhuTrinh <tatrinh0102@gmail.com>
2025-04-22 21:57:43 +0700

    đảy lại

---
+++ DATN_MVC/Controllers/QuanLyNhapHangController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DATN_MVC.Controllers
+{
+	public class QuanLyNhapHangController : Controller
+	{
+		public IActionResult QuanKho()
+		{
+			return View();
+		}
+	}
+}
